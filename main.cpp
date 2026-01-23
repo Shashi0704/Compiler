cc33186b4fcac4dba4f48bc55e878a692674574d
Shashi Pandey <144527893+Shashi0704@users.noreply.github.com>
2026-01-24 02:01:30 +0530

    Implement main function for parsing and displaying code

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "common.h"
+
+vector<Token> lexer(const string&);   // from lexer.cpp
+extern vector<IC> intermediateCode;
+
+int main() {
+    vector<Token> tokens = lexer("test.asm");
+
+    Parser parser(tokens);
+    parser.parseProgram();
+
+    cout << "\nINTERMEDIATE CODE\n";
+    for (auto &ic : intermediateCode) {
+        cout << "(" << ic.opcode << ", "
+             << ic.op1 << ", "
+             << ic.op2 << ")\n";
+    }
+
+    return 0;
+}
