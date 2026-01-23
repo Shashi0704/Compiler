2ae7c2d95df19826d6cc969cbd237db3bfa552d3
Shashi Pandey <144527893+Shashi0704@users.noreply.github.com>
2026-01-24 01:56:52 +0530

    Add common.h header file with token structures

---
+++ common.h
@@ -0,0 +1,37 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+enum TokenType {
+    INSTRUCTION,
+    REGISTER,
+    DIRECTIVE,
+    IDENTIFIER,
+    NUMBER,
+    SYMBOL
+};
+
+struct Token {
+    TokenType type;
+    string value;
+};
+
+struct Operand {
+    string value;
+};
+
+struct Instruction {
+    string opcode;
+    vector<Operand> operands;
+};
+
+struct IC {
+    string opcode;
+    string op1;
+    string op2;
+};
+
+#endif
